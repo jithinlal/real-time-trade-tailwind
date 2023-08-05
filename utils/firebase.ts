7dcbf2a125c24d0b0da4e8b3a5b13d0bbc03766f
Jithinlal <jithtitan007@gmail.com>
2023-08-06 00:29:46 +0530

    chore: analytics enabled

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,27 +1,27 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
-import "firebase/compat/analytics";
-import "firebase/compat/performance";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/analytics';
+import 'firebase/compat/performance';
 
 const config = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+	apiKey: process.env.NEXT_PUBLIC_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(config);
+	firebase.initializeApp(config);
 
-  // if (typeof window !== undefined) {
-  //   if ("measurementId" in config) {
-  //     firebase.analytics();
-  //     firebase.performance();
-  //   }
-  // }
+	if (typeof window !== undefined) {
+		if ('measurementId' in config) {
+			firebase.analytics();
+			firebase.performance();
+		}
+	}
 }
 
 const firestore = firebase.firestore();
